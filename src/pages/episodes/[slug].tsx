3c7af2a4d8f72d1fcff603a3eeb400e21ff6d5ed
Victor Powilleit <victor.powilleit@gmail.com>
2021-04-22 03:30:44 -0300

    Terceiro Dia

---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,103 @@
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Link from 'next/link'
+import Image from 'next/image'
+import database from '../../server/firebase'
+import convertDurationToTimeString from '../../utils/convertDurationToTimeString';
+
+import styles from './episode.module.scss'
+
+type Episode = {
+    id: string;
+    title: string;
+    thumbnail: string;
+    description: string;
+    members: string;
+    duration: number;
+    durationAsString: string;
+    url: string;
+    publishedAt: string;
+    // ...
+}
+
+type EpisodeProps = {
+    episode: Episode;
+}
+
+export default function Episode({ episode }: EpisodeProps) {
+    // const router = useRouter();
+    // const slug =  router.query.slug
+    // return <h1>{slug}</h1>
+    return (
+        <div className={styles.episode}>{episode.title}
+            <div className={styles.thumbnailContainer}>
+                <Link href="/">
+                    <button type="button">
+                        <img src="/arrow-left.svg" alt="Voltar" />
+                    </button>
+                </Link>
+                <Image width={700} height={160} src={episode.thumbnail} objectFit="cover" />
+                <button type="button">
+                    <img src="/play.svg" alt="Tocar episódio" />
+                </button>
+            </div>
+
+            <header>
+                <h1>{episode.title}</h1>
+                <span>{episode.members}</span>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+            </header>
+
+            <div
+                className={styles.description}
+                dangerouslySetInnerHTML={{ __html: episode.description }}
+            />
+        </div>
+    )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const { slug } = ctx.params
+
+    const data = await database.ref('episodes')
+        .orderByChild('id')
+        .equalTo(`${slug}`)
+        .once('value')
+        .then(function (snapshot) {
+            let resArray = []
+            snapshot.forEach((childSnapshot) => { resArray.push(childSnapshot.val()) })
+            const episodes = resArray.map(episode => {
+                return {
+                    order: episode.order,
+                    id: episode.id,
+                    title: episode.title,
+                    thumbnail: episode.thumbnail,
+                    members: episode.members,
+                    publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+                    duration: Number(episode.file.duration),
+                    durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+                    description: episode.description,
+                    url: episode.file.url
+                }
+            })
+            return episodes[0]
+        })
+
+    const episode = {
+        ...data
+    }
+
+    return {
+        props: { episode },
+        revalidate: 3600 //24h
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,139 @@
 import Link from 'next/link'
-export default function Home() {
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import database from '../server/firebase'
+import convertDurationToTimeString from '../utils/convertDurationToTimeString'
+import styles from './home.module.scss'
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+  // ...
+}
+
+type HomeProps = {
+  latestEpisodes: Episode[],
+  allEpisodes: Episode[]
+}
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <>
-      <h1>Index</h1>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos Lançamentos</h2>
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <Image
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  objectFit='cover'
+                />
+                <div className={styles.episodeDetails}>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+                <button>
+                  <img src="/play-green.svg" alt="Tocar episódio" />
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{ width: 72 }}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="/play-green.svg" alt="Tocar Episódio" />
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
       <Link href="/firebase">Firebase</Link>
-      <br/>
+      <br />
       <Link href="/ssr">SSR</Link>
-    </>
+    </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await database.ref('episodes')
+    .limitToFirst(12)
+    .once('value')
+    .then(function (snapshot) {
+      let resArray = []
+      snapshot.forEach((childSnapshot) => { resArray.push(childSnapshot.val()) })
+      const episodes = resArray.map(episode => {
+        return {
+          order: episode.order,
+          id: episode.id,
+          title: episode.title,
+          thumbnail: episode.thumbnail,
+          members: episode.members,
+          publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+          duration: Number(episode.file.duration),
+          durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+          description: episode.description,
+          url: episode.file.url
+        }
+      })
+      return episodes
+    })
+  const latestEpisodes = response.slice(0, 2)
+  const allEpisodes = response.slice(2, response.length)
+  return {
+    props: { latestEpisodes, allEpisodes }, revalidate: 1 //1 sec
+  }
 }
